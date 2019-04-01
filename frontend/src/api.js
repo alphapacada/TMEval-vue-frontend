e099a66234599dbb2cc3290246e3bb07d0dd2267
alpha <faipacada@gmail.com>
2019-04-01 18:39:10 +0800

    Celery integration in PredictionResult.vue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:5000';
 let $axios = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'http://localhost:5000/api/v1/',
   timeout: 5000,
   headers: {'Content-Type': 'application/json'}
 })
@@ -20,5 +20,32 @@ export default {
     fetchSecureResource () {
       return $axios.get(`secure-resource/zzz`)
         .then(response => response.data)
-    }
+    },
+    getLongTask(task_id) {
+      return $axios.get(`longtask/${task_id}`)
+        .then(response => response.data)
+    },
+    postLongTask() {
+      return $axios.post('longtask/')
+        .then(response => response.data)
+
+    },
+    //github.com/axios/axios#handling-errors
+    // axios.put(this.apiBaseEndpoint + '/' + id, input)
+    //   .then((response) => {
+
+    //   })
+    //   .catch(error) => {
+    //     if (error.response) {
+
+    //     } else if (error.request) {
+    //       console.log(error.request)
+    //     } else {
+    //       console.log('Error', error.message);
+
+    //     }
+    //     console.log(error.config)
+    //   });
+    
+
   }
--- frontend/src/views/PredictionResult.vue
+++ frontend/src/views/PredictionResult.vue
@@ -2,20 +2,85 @@
     <section class="section bg-secondary section-lg">
          <div class="container">
             <job-section></job-section>
-            <base-progress type="success" :value="60" label="Prediction Job completed"></base-progress>
+            <base-button @click="postLongTask()">Start Long Calculation</base-button>
+            <div id="progress">
+                 <div class='text-middle'> {{ percent }} </div>
+                <base-progress type="success" :value="progress_value" :label="job_status"></base-progress>
+                <div> {{ status }} </div>
+                <div> {{ result }} </div>
+            </div>
+           
          </div>
     </section>
     
 </template>
 
 <script>
+import $backend from '../api'
 import JobSection from "@/views/components/JobSection.vue";
 export default {
     name: "PredictionResult",
+    data() {
+        return {
+        job_status:'',
+        progress_value:0,
+        percent: 0,
+        status: '',
+        result: '',
+        }
+      
+
+    },
     components:{
         JobSection
+    },
+    methods: {
+        postLongTask() {
+             $backend.postLongTask()
+             .then(responseData => {
+                 console.log(responseData)
+                 //let status_url = responseHeaders['location'];
+                 let task_id = responseData['task_id']
+                 
+                 this.updateProgress(task_id)
+             })
+
+        },
+        updateProgress(task_id) {
+            console.log(task_id)
+            $backend.getLongTask(task_id) 
+            .then(responseData => {
+                console.log(responseData)
+                let percent = parseInt(responseData['current'] * 100 / responseData['total']);
+                // console.log(percent)
+                this.progress_value = percent;
+                this.percent = percent;
+                this.status = responseData['status'];
+                this.job_status = "Prediction Job: " + responseData['state']
+                if (responseData['state'] != 'PENDING' && responseData['state'] != 'PROGRESS') {
+                     if ('result' in responseData) {
+                    this.result = 'Result' + responseData['result'];
+                    } else {
+                        this.result = 'Result' + responseData['state'];
+                    }
+                }
+                else {
+                    setTimeout( () =>{
+                        this.updateProgress(task_id);
+                    },2000);
+                }
+               
+
+            })
+           
+
+        },
+       
+        // .then(response => (this.message = response))
     }
 }
+
+
 </script>
 
 

e8f642250fd316450d4e7c5933495c9af8ae510a
Alpha <faipacada@gmail.com>
2020-01-30 15:16:02 +0800

    added component for captioned table

---
+++ frontend/src/views/components/CaptionedTable.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="row pb-3">
+        <div class="col">
+            <div class="row">
+                <div class="text-center mx-auto col-xs-12 col-md-8 table-responsive">
+                    <div @click="handleClick" class="table-html" v-html="src"></div>
+                </div>
+            </div>
+            <div v-if="Boolean(text)" class="row">
+                <div v-resize-text="{minFontSize: '9px', maxFontSize: '16px'}" class="mx-auto p-3 col-xs-12 col-md-8 bg-secondary">
+                    {{ text }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+import {BImg} from "bootstrap-vue/esm/components/image/img";
+export default {
+    components: {
+        BImg,
+
+    },
+    name:"caption-table",
+    props:{
+        //Image source
+        src: String,
+        //Text below image
+        text: String,
+
+    },
+    methods: {
+        handleClick(evt) {
+        console.log("evt", evt)
+        this.$emit("clicked")
+    }
+  }
+};
+</script>
+<style scoped>
+table {
+  width: auto !important;
+}
+.table-html {
+  display: inline-block;
+}
+
+</style>
